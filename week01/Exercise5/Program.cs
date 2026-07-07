bca3271103ac615b25bc2271d973287aff3ddac5
fredbern7 <fredbern7@gmail.com>
2026-07-08 02:57:28 +1100

    second update week01

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int squareRoot = SquareNumber(number);
+
+        DisplayResult(userName, squareRoot);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name here: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your name here: ");
+        string number = Console.ReadLine();
+        int favNumber = int.Parse(number);
+        return favNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int result = number * number;
+        return result;
+    }
+
+    static void DisplayResult(string userName, int squareRoot)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareRoot}");
+    }
+
+
 }
